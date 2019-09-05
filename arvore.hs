498665672454241fccb1b500b0f2e40c0dac233b
ux14 <ebrosa@inf.ufrgs.br>
2019-09-04 23:25:23 -0300

    Add files via upload

---
+++ arvore.hs
@@ -0,0 +1,26 @@
+data Tree = EmptyTree | Tree {root :: Int, left :: Tree, right :: Tree}
+
+singleton :: Int -> Tree
+singleton x = Tree x EmptyTree EmptyTree
+
+findInTree :: Int -> Tree -> Bool
+findInTree _ EmptyTree = False
+findInTree x tree = (x == (root tree)) || (findInTree x (left tree)) || (findInTree x (right tree))
+
+uniqueNumAux :: (Tree,Int) -> (Tree,Int)
+uniqueNumAux (EmptyTree,curr) = (EmptyTree,curr)
+uniqueNumAux (tree,curr) = (Tree next renumTreeLeft renumRightTree, last)
+ where 
+ next = curr + 1
+ aux1 = uniqueNumAux (left tree, next)
+ aux2 = uniqueNumAux (right tree, snd $ aux1)
+ last = snd aux2
+ renumTreeLeft = fst $ aux1
+ renumRightTree = fst $ aux2
+
+uniqueNum :: Tree -> Tree
+uniqueNum t = fst $ uniqueNumAux (t,0)
+
+listFromTree :: Tree -> [Int]
+listFromTree EmptyTree = []
+listFromTree tree = (root tree) : (listFromTree $ left tree) ++ (listFromTree $ right tree)
---
+++ teste.hs
@@ -0,0 +1,121 @@
+type Node  = Int
+type Edge  = Int
+type Path = [Edge]
+
+data Graph = Graph { nodes  :: [Node]
+					,edges  :: [Edge]
+					,source :: Edge -> Node
+					,target :: Edge -> Node}
+
+-- Constroi a função 'source' de um grafo a partir de sua
+-- Lista de adjacência
+sourceFromAdjList :: [(Edge,Node,Node)] -> (Edge -> Node)
+sourceFromAdjList adjL = \e -> ((\(_,x,_) -> x) . (findTupleWithEdge e) $ adjL)
+ where
+ findTupleWithEdge e = head . filter (\(ei,_,_) -> ei == e)
+
+-- Constroi a função 'target' de um grafo a partir de sua
+-- Lista de adjacência
+targetFromAdjList :: [(Edge,Node,Node)] -> (Edge -> Node)
+targetFromAdjList adjL = \e -> ((\(_,_,x) -> x) . (findTupleWithEdge e) $ adjL)
+ where
+ findTupleWithEdge e = head . filter (\(ei,_,_) -> ei == e)
+
+-- Lista de arestas que tem o nodo 'n' como fonte (source)
+srcNode :: Graph -> Node -> [Edge]
+srcNode g n = filter (\e -> source g e == n) $ edges g
+
+-- Lista de arestas que tem o nodo 'n' como alvo (target)
+tgtNode :: Graph -> Node -> [Edge]
+tgtNode g n = filter (\e -> target g e == n) $ edges g
+
+-- Dado uma lista de arestas, tenta extender com a aresta 'e'
+-- Possível falha: alvo da última aresta é diferente da fonte
+-- de 'e'
+extendPath :: Graph -> Edge -> Path -> Path
+extendPath g e [] = []
+extendPath g e (ei:ek)
+ | target g ei == source g e = e:ei:ek
+ | otherwise = []
+
+-- Trata uma lista de caminhos como uma computação não-determinística
+-- e usa de propriedades de listas como Funtores Aplicativos e listas
+-- como uma Monada, extendendo cada caminho por todas as arestas que
+-- tem como fonte o alvo da última aresta no caminho (através de extendPath)
+extendListOfPaths :: Graph -> [Path] -> [Path]
+extendListOfPaths g paths = filter (\p -> p /= []) $ paths >>= (\p -> (extendPath g) <$> (edges g) <*> [p])
+
+findNonRepPaths :: Graph -> Node -> Node -> [Path]
+findNonRepPaths g ni nj = concat (caminhos g ni nj)
+
+-- lista de lista de caminhos em que cada elemento é uma lista
+-- de todos os caminhos com um número fixo de arestas de 1 até |e| 
+-- arestas que começam em uma aresta com fonte ni e terminam em uma
+-- aresta com alvo nj
+caminhos :: Graph -> Node -> Node -> [[Path]]
+caminhos g ni nj = map (caminho g ni nj) $ take (length $ edges g) $ [1..]
+
+-- lista de caminhos com k arestas em que a primeira aresta 
+-- tem como fonte ni e a última aresta tem como alvo nj 
+-- e todas arestas tem como fonte o alvo da anterior
+caminho :: Graph -> Node -> Node -> Int -> [Path]
+caminho g ni nj k = filter (\(ei:ek) -> target g ei == nj) (pathsWithLength g (srcNode g ni) k)
+
+-- Lista de caminhos que começam por alguma aresta
+-- de 'ini' e tem k arestas.
+pathsWithLength :: Graph -> [Edge] -> Int -> [Path]
+pathsWithLength g ini k
+ | k <= 1 = map (\e->[e]) ini
+ | otherwise = extendListOfPaths g $ pathsWithLength g ini (k-1)
+
+------------------------------------------------------------------------
+
+-- Morfismo entre dois grafos graph1 -> graph2
+data GraphMorphism = GraphMorphism { src :: Graph
+									,tgt :: Graph
+									,fv  :: Node -> Node
+									,fe  :: Edge -> Edge}
+
+isHomomorphism :: GraphMorphism -> Bool
+isHomomorphism gm = (commutes ( (fv gm) . source1) (source2 . (fe gm) ) e1) && (commutes ( (fv gm) . target1) (target2 . (fe gm) ) e1)
+ where
+ commutes fi fj e = (map fi e) == (map fj e)
+ (g1,g2) = (src gm, tgt gm)
+ (e1,source1,target1) = (edges g1, source g1, target g1)
+ (e2,source2,target2) = (edges g2, source g2, target g2)
+
+-- find
+find :: (a -> Bool) -> [a] -> Maybe a
+find _ [] = Nothing
+find f (xi:x) = if (f xi) then (Just xi) else (find f x)
+
+-- Função a partir de uma lista associativa.
+assocListToFunc :: (Eq a) => [(a,b)] -> (a -> Maybe b)
+assocListToFunc l a = let maybePair = find (\p -> fst p == a) l
+						in 
+						fmap (\p -> snd p) maybePair
+
+-- Dada uma lista representando um conjunto,
+-- retorna todas as listas feitas de elementos
+-- desse conjunto com o tamanho da lista de 
+-- elementos. (l^l elementos)
+--
+-- Exemplo: genAllListsFromSet [1,2] = [ [1,1], [1,2], [2,1], [2,2] ]
+--
+genAllListsFromSet :: [a] -> [[a]]
+genAllListsFromSet l = all n
+ where
+ n = length l
+ listOfLists n = take n $ map (\x -> l) (repeat n)
+ all n = foldl (\x y -> (\z w -> z:w) <$> y <*> x) ([[]]) (listOfLists n)
+
+-- Gera uma lista associativa a partir de duas
+-- listas.
+genAssocList :: [a] -> [b] -> [(a,b)]
+genAssocList = zipWith (\f s -> (f,s))
+
+genAllAssocList :: [a] -> [b] -> [[(a,b)]]
+genAllAssocList a b = map (genAssocList a) (genAllListsFromSet b)
+
+allFuncs :: [a] -> [b] -> [(a -> Maybe b)]
+allFuncs a b = map assocListToFunc (genAllAssocList a b)
